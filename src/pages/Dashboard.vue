393974a5752933b3e4653cf7491616526e5d796f
Victor Edet <edetvictor849@gmail.com>
2022-05-13 08:00:45 +0100

    (NIMI)Chore: new facelift dashboard

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,132 +1,88 @@
 <template>
-    <div class="dashboard d-flex">
-        <div class="d-none d-md-block col-2">
-            <Nav/>
-        </div>
-        <div class="col-md-10 container pb-5 content vh-100">
-             <div class="col-12 bg-primary  card shadow-sm m-2">
-                <iframe src="https://bit2me.com/widget/crypto-carousel/v1" style="display:block;width:100%;height:40px;" frameborder="0"></iframe>
-            </div>
-             <div class="col-12 bg-primary animate__animated animate__bounce  card shadow-sm m-2">
-                <h4 class="text-white p-2">Deligence with AI powerred trade</h4>
-            </div>
-            <div class="row col-12 py-2">
-                <div class="col-md-3 m-2">
-                    <div class="py-2 container bg-white border-5 border-bottom border-primary rounded shadow">
-                        <p class="fw-bold">Account Balance</p>
-                        <h3>${{this.$store.state.user.account}}</h3>
-                    </div>
-                </div>
-                 <div class="col-md-3 m-2">
-                    <div class="py-2 container bg-white border-5 border-bottom border-primary rounded shadow">
-                        <p class="fw-bold">Profit Balance</p>
-                        <h3>${{this.$store.state.user.profit}}</h3>
-                    </div>
-                </div>
-                 <div class="col-md-3 m-2">
-                    <div class="py-2 container bg-white border-5 border-bottom border-primary rounded shadow">
-                        <p class="fw-bold">Active Signals</p>
-                        <h3> {{signals.length}} </h3>
-                    </div>
-                </div>
-                <div class="col-md-3 m-2">
-                    <div class="py-2 container bg-white border-5 border-bottom border-primary rounded shadow">
-                        <p class="fw-bold">Active Contracts</p>
-                        <h3> {{plans.length}} </h3>
-                    </div>
-                </div>
-                  
-               
-
-            </div>
-            <div class="chart container bg-white col-12 pb-5">
-                <div class="col-12">
-                    <iframe src="https://bit2me.com/widget/chart/v1?currency=B2M&fiat=USDT" style="display:block;width:100%;height:400px;margin:0 auto;" frameborder="0"></iframe>
-                </div>
-                <div class=" py-1 bg-primary container">
-                    <p class="m-1 fw-bold fs-5 col-12 text-white text-center">Trade Statistics</p> 
-                </div>
-                <div class="col-12">
-                    <div v-for="coin in coins" :key="coin.id" class="d-flex border-1 border">
-                        <div class="text-center col-2 m-1"><img :src="coin.image" class="w-25" /> </div>
-                        <div class="col-4 m-1 fw-bold">{{coin.name}}</div>
-                        <div class="col-3 m-1">${{coin.current_price}}</div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <Bottom/>
-    </div>
+<div class="body">
+	<div class="nav-top text-center text-white">
+		
+	</div>
+	<div class="nav py-2  d-flex justify-content-between">
+		<div class="col-2">
+			<h1 class="m-2 text-white i">A1TradeFx</h1>
+		</div>
+		<div class="col-4"></div>
+		<div class="col-2 m-2">
+			<div class="border-primary rounded border text-white d-flex col-9 ">
+				<div class="p-2">
+					<svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" color="#e42575" height="24" width="24" xmlns="http://www.w3.org/2000/svg" style="color: rgb(228, 37, 117);"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
+				</div>
+				<span class="col btn__primary d-none d-md-block p-2"> My profile</span>
+			</div>
+		</div>
+	</div>
+	<div class="d-flex">
+		<div class="d-none d-md-block col-2  vh-100 border-5 border-end">
+			<div class="container text-primary ">
+				<div class="p-1 my-2 border-1 border rounded side-link">
+					<p class="m-1 fw-bold text-white">Dashboard</p>
+				</div>
+				<div class="p-1 my-2 border-1 border rounded side-link">
+					<p class="m-1 fw-bold text-white">Deposit</p>
+				</div>
+				<div class="p-1 my-2 border-1 border rounded side-link">
+					<p class="m-1 fw-bold text-white">Withdraw</p>
+				</div>
+				<div class="p-1 my-2 border-1 border rounded side-link">
+					<p class="m-1 fw-bold text-white">Contracts</p>
+				</div>
+				<div class="p-1 my-2 border-1 border rounded side-link">
+					<p class="m-1 fw-bold text-white">History</p>
+				</div>
+				<div class="p-1 my-2 border-1 border rounded side-link">
+					<p class="m-1 fw-bold text-white">Log Out</p>
+				</div>
+ 			</div>
+		</div>
+		<div class="col">
+			<div class="col-12 text-center"><p class="text-primary"><span class="fa fa-clock"></span> {{new_now}}*</p> </div>
+			 <div class="container row">
+				 <div class="col-md-5 m-2 py-2 text-white shadow border border-white rounded">
+					<div class="col-6">
+						<p>Account Balance:</p>
+						<h3>$7,000  </h3>
+					</div>					 
+				 </div>
+				  <div class="col-md-5 text-white m-2 py-2 border border-white rounded">
+					 <p>Active Contracts:</p>
+					 <h3>6</h3>
+				 </div>
+			 </div>
+		</div>
+		
+	</div> 
+</div>
 </template>
-
 <script>
-import Nav from '../components/Nav.vue'
+import moment from 'moment'
 import axios from 'axios'
-import Bottom from '../components/Bottom.vue'
 
-
-export default{
-    name:'Dashboard',
-    data(){
-        return{
-            coins:{},
-            signals:{},
-            plans:{}
-        }
-    },
-     components:{
-    Nav,
-    Bottom
-},
-    async mounted(){
-
-         await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1&sparkline=false').then(res=>{
-            this.coins = res.data
-        })
-
-        await axios.get('https://www.app.a1tradefx.com/backend/public/api/my_signals/'+this.$store.state.user.id,{
-        headers:{
-          Authorization: `Bearer ${this.$store.state.token}`
-        }
-      }).then((res)=>{
-        this.signals=res.data
-      })
-
-       await axios.get('https://www.app.a1tradefx.com/backend/public/api/my_plans/'+this.$store.state.user.id,{
-        headers:{
-          Authorization: `Bearer ${this.$store.state.token}`
-        }
-      }).then((res)=>{
-        this.plans=res.data
-      })
-  
-        if(!this.$store.state.user.name){
-            this.$router.push('/login')
-        }
-         await axios.get('https://www.app.a1tradefx.com/backend/public/api/user/'+this.$store.state.user.id,{
-               headers:{
-                Authorization: `Bearer ${this.$store.state.token}`
-                }
-          }).then((res)=>{
-              let user = res.data
-              this.$store.commit('updateUserDetails', user)
-          })
-    }
-   
+export default {
+	data(){
+		return{
+			new_now:'',
+			nfts:{}
+ 		}
+	},
+	async mounted(){
+		this.updateTime()
+		await axios.get().then((res)=>{
+			this.nfts = res.data
+		})
+	},
+	methods:{
+		updateTime(){
+			setInterval(this.currentTime,1000)
+		},
+		currentTime(){
+			this.new_now = moment([])
+		}
+	}
 }
 </script>
-<style scoped>
- .content{
-    overflow: scroll;
-}
-
- 
-.content::-webkit-scrollbar {
-  display: none;
-}
-
-.content {
-  -ms-overflow-style: none;  
-  scrollbar-width: none;  
-} 
-</style>
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  optimizeDeps:{
+    exclude:['vendor']
+  },
   plugins: [vue()]
 })
